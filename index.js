a313ca23927c7466dbed4a4107bd9de68220d5e4
akhil-byte-oops <akhileshp6336@gamil.com>
2026-09-18 19:54:22 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
